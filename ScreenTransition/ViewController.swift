2f5789b42d1a79ae447684e4a0e97b9a8a2a127b
serijun1214 <serijun.1214+github@gmail.com>
2024-09-10 00:19:39 +0900

    Initial Commit

---
+++ ScreenTransition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenTransition
+//
+//  Created by 瀬利純樹 on 2024/09/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
